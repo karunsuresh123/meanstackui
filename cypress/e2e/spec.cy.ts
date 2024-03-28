800daf41409b612f699d9e0c2439de10eca7a35f
Karun Suresh Raj <karun.suresh@gmail.com>
2024-03-27 20:43:49 -0700

    initial commit

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('MEAN Stack')
+    cy.contains('MEAN Stack Example')
     cy.contains('ToDo List Demo')
   })
 })
